f62e9b10edcc3b902e464ebe29f9d5307879dd11
Vinayak Gupta <vinayakg2001@gmail.com>
2024-03-07 18:12:32 +0530

    First Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        sum(a,b);
+    }
+
+    public static void sum(int a,int b){
+        System.out.println(a+b);
+    }
+}
